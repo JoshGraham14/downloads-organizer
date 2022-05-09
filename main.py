d81ef9e3cd5e0ab89ccf66ed707821db11e5bee6
Josh Graham <joshgraham14@gmail.com>
2022-05-08 20:50:10 -0400

    :sparkles: Added constants for image file types and sort_folders function definition

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 # Replace with absolute path to Downloads folder
 BASE_PATH: str = 'test_folder'
+IMG_FILE_TYPES: list[str] = ['png', 'jpg', 'jpeg', 'gif', 'raw', 'svg']
+
 
 def check_folders() -> list[str]:
     '''Return a list of needed folders for the Downloads directory.'''
@@ -13,9 +15,12 @@ def check_folders() -> list[str]:
         needed_folders.append('Images')
     if 'Executables' not in directory:
         needed_folders.append('Executables')
-    
-    return needed_folders
+    if 'Zipped' not in directory:
+        needed_folders.append('Zipped')
+    if 'PDFs' not in directory:
+        needed_folders.append('PDFs')
 
+    return needed_folders
 
 
 def create_folders(needed_folders: list[str]) -> None:
@@ -24,12 +29,16 @@ def create_folders(needed_folders: list[str]) -> None:
         os.mkdir(folder)
 
 
+def sort_folders() -> None:
+    pass
+
+
 def main():
-    print(os.listdir())
     os.chdir(BASE_PATH)
     needed_folders: list[str] = check_folders()
     create_folders(needed_folders)
     print(os.listdir())
+
 
 if __name__ == '__main__':
     main()
